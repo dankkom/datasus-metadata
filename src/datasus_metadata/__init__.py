f108a6c5fb264850eb1a89b272f7b35948f679e4
Daniel Komesu <danielkomesu@gmail.com>
2024-11-09 13:51:38 -0300

    Update

--- src/datasus_metadata/__init__.py
+++ src/datasus_metadata/__init__.py
@@ -6,11 +6,16 @@ from datasus_fetcher.fetcher import connect, list_dataset_files, list_files
 from datasus_fetcher.meta import auxiliary_tables, datasets, docs
 
 
-def update_files(metadata_dir_path: Path):
+def save_json(data: dict, filepath: Path):
+    with open(filepath, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=1, default=str, ensure_ascii=False)
+
+
+def update_data_files(metadata_dir_path: Path):
     metadata_dir_path.mkdir(parents=True, exist_ok=True)
     ftp = connect()
     for dataset in datasets:
-        print("Listing files of", dataset)
+        print("Listing data files of", dataset)
         data = []
         for remote_file in list_dataset_files(ftp, dataset):
             data.append(
@@ -29,12 +34,8 @@ def update_files(metadata_dir_path: Path):
                     },
                 }
             )
-        if not data:
-            print(f"{dataset}: No files found")
-            return
         metadata_file_path = metadata_dir_path / f"{dataset}.json"
-        with open(metadata_file_path, "w", encoding="utf-8") as f:
-            json.dump(data, f, indent=1, default=str, ensure_ascii=False)
+        save_json(data, metadata_file_path)
 
 
 def list_documentation_files(ftp: ftplib.FTP, dataset: str) -> list[dict]:
@@ -48,7 +49,7 @@ def update_docs(metadata_dir_path: Path):
     metadata_dir_path.mkdir(parents=True, exist_ok=True)
     ftp = connect()
     for doc in docs:
-        print("Listing files of", doc)
+        print("Listing documentation files of", doc)
         data = []
         for file in list_documentation_files(ftp, doc):
             data.append(
@@ -61,12 +62,8 @@ def update_docs(metadata_dir_path: Path):
                     "dataset": doc,
                 }
             )
-        if not data:
-            print(f"{doc}: No files found")
-            return
         metadata_file_path = metadata_dir_path / f"{doc}.json"
-        with open(metadata_file_path, "w", encoding="utf-8") as f:
-            json.dump(data, f, indent=1, default=str, ensure_ascii=False)
+        save_json(data, metadata_file_path)
 
 
 def list_auxiliary_tables_files(ftp: ftplib.FTP, dataset: str) -> list[dict]:
@@ -80,7 +77,7 @@ def update_aux(metadata_dir_path: Path):
     metadata_dir_path.mkdir(parents=True, exist_ok=True)
     ftp = connect()
     for aux in auxiliary_tables:
-        print("Listing files of", aux)
+        print("Listing auxiliary files of", aux)
         data = []
         for file in list_auxiliary_tables_files(ftp, aux):
             data.append(
@@ -93,9 +90,83 @@ def update_aux(metadata_dir_path: Path):
                     "dataset": aux,
                 }
             )
-        if not data:
-            print(f"{aux}: No files found")
-            return
         metadata_file_path = metadata_dir_path / f"{aux}.json"
-        with open(metadata_file_path, "w", encoding="utf-8") as f:
-            json.dump(data, f, indent=1, default=str, ensure_ascii=False)
+        save_json(data, metadata_file_path)
+
+
+def get_partition_periods(files: list[dict]) -> set[str]:
+    periods = set()
+    for file in files:
+        period = f"{file['partition']['year']}"
+        if file["partition"]["month"]:
+            period += f"-{file['partition']['month']:0>2}"
+        periods.add(period)
+    return periods
+
+
+def update_index(metadata_dir_path: Path):
+    metadata_dir_path.mkdir(parents=True, exist_ok=True)
+    metadata_index = {
+        "data": {},
+        "documentation": {},
+        "auxiliary": {},
+    }
+
+    for metadata_filepath in sorted((metadata_dir_path / "data").glob("*.json")):
+        dataset = metadata_filepath.stem
+        with open(metadata_filepath, "r", encoding="utf-8") as f:
+            data = json.load(f)
+        n_files = len(data)
+        total_size = sum(file["size"] for file in data)
+        ufs = set(file["partition"]["uf"] for file in data)
+        n_partition_ufs = len(ufs)
+        partition_periods = get_partition_periods(data)
+        first_period = min(partition_periods) if partition_periods else None
+        last_period = max(partition_periods) if partition_periods else None
+        n_partition_periods = len(partition_periods)
+        if all(len(date) == 7 for date in partition_periods):
+            partition_periodicity = "monthly"
+        else:
+            partition_periodicity = "yearly"
+        latest_update = max(file["datetime"] for file in data) if data else None
+        metadata_index["data"][dataset] = {
+            "n_files": n_files,
+            "total_size": total_size,
+            "partition_n_ufs": n_partition_ufs,
+            "partition_period_start": first_period,
+            "partition_period_end": last_period,
+            "partition_n_periods": n_partition_periods,
+            "partition_periodicity": partition_periodicity,
+            "latest_update": latest_update,
+        }
+
+    for metadata_filepath in sorted(
+        (metadata_dir_path / "documentation").glob("*.json")
+    ):
+        dataset = metadata_filepath.stem
+        with open(metadata_filepath, "r", encoding="utf-8") as f:
+            data = json.load(f)
+        n_files = len(data)
+        total_size = sum(file["size"] for file in data)
+        latest_update = max(file["datetime"] for file in data)
+        metadata_index["documentation"][dataset] = {
+            "n_files": n_files,
+            "total_size": total_size,
+            "latest_update": latest_update,
+        }
+
+    for metadata_filepath in sorted((metadata_dir_path / "auxiliary").glob("*.json")):
+        dataset = metadata_filepath.stem
+        with open(metadata_filepath, "r", encoding="utf-8") as f:
+            data = json.load(f)
+        n_files = len(data)
+        total_size = sum(file["size"] for file in data)
+        latest_update = max(file["datetime"] for file in data)
+        metadata_index["auxiliary"][dataset] = {
+            "n_files": n_files,
+            "total_size": total_size,
+            "latest_update": latest_update,
+        }
+
+    metadata_index_path = metadata_dir_path / "index.json"
+    save_json(metadata_index, metadata_index_path)
--- update-metadata.py
+++ update-metadata.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 
-from datasus_metadata import update_aux, update_docs, update_files
+from datasus_metadata import update_aux, update_docs, update_data_files, update_index
 
 if __name__ == "__main__":
-    update_files(Path("metadata/files"))
+    update_data_files(Path("metadata/data"))
     update_docs(Path("metadata/documentation"))
     update_aux(Path("metadata/auxiliary"))
+    update_index(Path("metadata"))
