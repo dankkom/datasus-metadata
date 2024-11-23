361084f4a373688b09e8aaf24c748767604d34a9
Daniel Komesu <danielkomesu@gmail.com>
2024-11-23 18:14:02 -0300

    Update script 'update-metadata.py'

--- update-metadata.py
+++ update-metadata.py
@@ -2,10 +2,12 @@ from pathlib import Path
 from datasus_fetcher.fetcher import connect
 from datasus_metadata import update_aux, update_docs, update_data_files, update_index
 
+
 if __name__ == "__main__":
+    metadata_dir = Path("metadata")
     ftp = connect()
-    update_data_files(ftp, Path("metadata/data"))
-    update_docs(ftp, Path("metadata/documentation"))
-    update_aux(ftp, Path("metadata/auxiliary"))
+    update_data_files(ftp, metadata_dir / "data")
+    update_docs(ftp, metadata_dir / "documentation")
+    update_aux(ftp, metadata_dir / "auxiliary")
     ftp.close()
-    update_index(Path("metadata"))
+    update_index(metadata_dir)
