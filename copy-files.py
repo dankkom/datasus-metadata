1ef32eeabd5fa57d7fae688c249ecfcd8e3365b6
Daniel Komesu <danielkomesu@gmail.com>
2024-11-23 19:04:59 -0300

    Fix script not creating directories

--- copy-files.py
+++ copy-files.py
@@ -5,9 +5,9 @@ from pathlib import Path
 if __name__ == "__main__":
     metadata_dir = Path("metadata")
     data_dir = Path("data")
-    data_dir.mkdir(exist_ok=True)
     for file in metadata_dir.glob("**/*.json"):
         dest_filename = file.name.replace("-", "_")
         dest_filepath = data_dir / file.relative_to(metadata_dir)
         dest_filepath = dest_filepath.with_name(dest_filename)
+        dest_filepath.parent.mkdir(parents=True, exist_ok=True)
         shutil.copy(file, dest_filepath)
